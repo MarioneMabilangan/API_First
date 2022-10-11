1749349874666975195507c01b81182bb5741aba
MarioneMabilangan <mbasaysa@jaumebalmes.net>
2022-10-11 15:37:25 +0200

    a

--- src/main/java/net/jaumebalmes/Control/de/horarios/Alumne.java
+++ src/main/java/net/jaumebalmes/Control/de/horarios/Alumne.java
@@ -6,6 +6,7 @@ public class Alumne {
     private String nom;
     private String cognoms;
     private LocalDate dataNaixement;
+    private String grup;
 
     public String getNom() {
         return nom;
@@ -25,11 +26,21 @@ public class Alumne {
     public void setDataNaixement(LocalDate dataNaixement) {
         this.dataNaixement = dataNaixement;
     }
-    public Alumne(String nom, String cognoms, LocalDate dataNaixement) {
+
+    public String getGrup() {
+        return grup;
+    }
+
+    public void setGrup(String grup) {
+        this.grup = grup;
+    }
+
+    public Alumne(String nom, String cognoms, LocalDate dataNaixement, String grup) {
         super();
         this.nom = nom;
         this.cognoms = cognoms;
         this.dataNaixement = dataNaixement;
+        this.grup = grup;
     }
 }
 
--- src/main/java/net/jaumebalmes/Control/de/horarios/ClientController.java
+++ src/main/java/net/jaumebalmes/Control/de/horarios/ClientController.java
@@ -1,6 +1,7 @@
 package net.jaumebalmes.Control.de.horarios;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +17,19 @@ public class ClientController {
     public List<Alumne> getAlumnes(){
         ArrayList<Alumne> Alumnes = new ArrayList<>();
 
-        Alumnes.add(new Alumne("Marione","Basaysay Mabilangan", LocalDate.of(2003, Month.JUNE, 4)));
-        Alumnes.add(new Alumne("Jesus","Cristo", LocalDate.of(2003, Month.JUNE, 4)));
-        Alumnes.add(new Alumne("Jose","Ricardo", LocalDate.of(2003, Month.JUNE, 4)));
+        Alumnes.add(new Alumne("Marione","Basaysay Mabilangan", LocalDate.of(2003, Month.JUNE, 4), "DAM2"));
+        Alumnes.add(new Alumne("Jesus","Cristo", LocalDate.of(2003, Month.JUNE, 4), "ASIX"));
+        Alumnes.add(new Alumne("Jose","Ricardo", LocalDate.of(2003, Month.JUNE, 4), "DAM2"));
         return Alumnes;
+    }
+
+    @GetMapping("/alumnes/{id}")
+    public Alumne getnAlumnes(@PathVariable int id){
+        ArrayList<Alumne> alumnes = new ArrayList<>();
+
+        alumnes.add(new Alumne("Marione","Basaysay Mabilangan", LocalDate.of(2003, Month.JUNE, 4), "DAM2"));
+        alumnes.add(new Alumne("Jesus","Cristo", LocalDate.of(2003, Month.JUNE, 4), "ASIX"));
+        alumnes.add(new Alumne("Jose","Ricardo", LocalDate.of(2003, Month.JUNE, 4), "DAM2"));
+        return alumnes.get(id);
     }
 }
