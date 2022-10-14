af28813c40372dfa6fce14178959cf47fe0c4e9e
jfrancis <jfrancis@jaumebalmes.net>
2022-10-14 16:59:03 +0200

    jose

---
+++ src/main/java/net/jaumebalmes/Control/de/horarios/Horas.java
@@ -0,0 +1,59 @@
+package net.jaumebalmes.Control.de.horarios;
+
+
+
+public class Horas {
+
+Alumne Alumnes;
+
+int UF;
+double totalHoras;
+double TotalFaltas;
+
+
+
+
+public Alumne getEstudiante() {
+	return Alumnes;
+}
+public void setEstudiante(Alumne Alumnes) {
+	this.Alumnes = Alumnes;
+}
+public int getUF() {
+	return UF;
+}
+public void setUF(int uF) {
+	UF = uF;
+}
+public double getTotalHoras() {
+	return totalHoras;
+}
+public void setTotalHoras(double totalHoras) {
+	this.totalHoras = totalHoras;
+}
+public double getTotalFaltas() {
+	return TotalFaltas;
+}
+public void setTotalFaltas(double totalFaltas) {
+	TotalFaltas = totalFaltas;
+}
+public Horas( Alumne Alumnes, int uF, double totalHoras, double totalFaltas) {
+	super();
+	this.Alumnes = Alumnes;
+	UF = uF;
+	this.totalHoras = totalHoras;
+	TotalFaltas = totalFaltas;
+}
+@Override
+public String toString() {
+	return "Horas [" + Alumnes + ", UF=" + UF + ", totalHoras=" + totalHoras + ", TotalFaltas="
+			+ TotalFaltas + "]";
+}
+
+
+
+
+
+
+
+}
